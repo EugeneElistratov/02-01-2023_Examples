d2bb36b365980fa7063d655eff544775f3309bb3
Евгений Елистратов <em_elistratov@mail.ru>
2023-01-05 12:41:53 +0300

    Исправил ошибки в программе

--- Example008_2broAndDog/Program.cs
+++ Example008_2broAndDog/Program.cs
@@ -1,24 +1,25 @@
 ﻿// Задача про двух друзей и собаку, которая бегает между ними,
 // пока они идут навстречу друг другу. Сколько раз собака пробежит между друзьями?
-double count = 0;
-double distance = 800;
+int count = 0;
+double distance = 80000;
 int firstFriendSpeed = 1;
 int secondFriendSpeed = 2;
 int dogSpeed = 5;
 int friend = 2;
 double time = 0;
-while(distance>10)
+while (distance > 10)
 {
-if(friend == 1);
+    if (friend == 1);
     {
     time = distance / (firstFriendSpeed + dogSpeed);
     friend = 2;
     }
+    {
     time = distance / (secondFriendSpeed + dogSpeed);
     friend = 1;
-    
-distance = distance - ((firstFriendSpeed + secondFriendSpeed) * time);
-Console.WriteLine(count);
-count++;
+    }
+    distance = distance - ((firstFriendSpeed + secondFriendSpeed) * time);
+    count++;
 }
-// Console.WriteLine(time);
+Console.Write("Собака пробежит " + count + " раз.");
+Console.WriteLine();
