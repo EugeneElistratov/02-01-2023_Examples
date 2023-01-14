a5411724ed831ceacb6397bbdfa3528c82504e50
Евгений Елистратов <em_elistratov@mail.ru>
2023-01-14 17:43:01 +0300

    Исправил цикл и добавил запись шагов

--- Example008_2broAndDog/Program.cs
+++ Example008_2broAndDog/Program.cs
@@ -9,17 +9,21 @@ int friend = 2;
 double time = 0;
 while (distance > 10)
 {
-    if (friend == 1)
-    {
-    time = distance / (firstFriendSpeed + dogSpeed);
-    friend = 2;
-    }
-    {
-    time = distance / (secondFriendSpeed + dogSpeed);
-    friend = 1;
-    }
-    distance = distance - ((firstFriendSpeed + secondFriendSpeed) * time);
-    count++;
+if (friend == 1)
+{
+time = distance / (firstFriendSpeed + dogSpeed);
+friend = 2;
+Console.WriteLine("time to firstFriend = " + time);
+}
+else
+{
+time = distance / (secondFriendSpeed + dogSpeed);
+friend = 1;
+Console.WriteLine("time to secondFriend = " + time);
+}
+distance = distance - ((firstFriendSpeed + secondFriendSpeed) * time);
+Console.WriteLine("distance = " + distance);
+count++;
 }
 Console.Write("Собака пробежит " + count + " раз.");
 Console.WriteLine();
