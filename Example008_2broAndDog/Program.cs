0c25c0e62c037d66867b77a82212ee480d6abe6c
Евгений Елистратов <em_elistratov@mail.ru>
2023-01-14 11:34:25 +0300

    Исправил дистанацию на 10 000.

--- Example008_2broAndDog/Program.cs
+++ Example008_2broAndDog/Program.cs
@@ -1,7 +1,7 @@
-﻿// Задача про двух друзей и собаку, которая бегает между ними,
-// пока они идут навстречу друг другу. Сколько раз собака пробежит между друзьями?
+﻿// Два друга идут навстречу друг другу - дистанция 10000, скорость: друг1 = 1, друг2 = 2, собака = 5. 
+// Сколько раз собака пробежит между друзьями?
 int count = 0;
-double distance = 80000;
+double distance = 10000;
 int firstFriendSpeed = 1;
 int secondFriendSpeed = 2;
 int dogSpeed = 5;
@@ -9,7 +9,7 @@ int friend = 2;
 double time = 0;
 while (distance > 10)
 {
-    if (friend == 1);
+    if (friend == 1)
     {
     time = distance / (firstFriendSpeed + dogSpeed);
     friend = 2;
